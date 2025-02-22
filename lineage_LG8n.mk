5048a10fcd4880f87cff46a072894cab24a63e2e
Shirayuki39 <lorddemecrius83@proton.me>
2025-02-22 10:49:50 +0800

    LG8n: AxionAOSP Bringup
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- lineage_LG8n.mk
+++ lineage_LG8n.mk
@@ -27,4 +27,21 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceName=LG8n \
     BuildFingerprint=TECNO/LG8n-GL/TECNO-LG8n:12/SP1A.210812.016/240531V2678:user/release-keys
 
+# AxionAOSP Flags
+AXION_CAMERA_REAR_INFO := 50
+AXION_CAMERA_FRONT_INFO := 8
+AXION_MAINTAINER := Shirayuki39
+AXION_PROCESSOR := Helio_G99
+PRODUCT_NO_CAMERA := false
+BYPASS_CHARGE_SUPPORTED := false
+TARGET_USES_USLMK := false
 
+# Enable activity open override fix for low-end devices or devices affected by activity open/exit freezing issue 
+PERF_ANIM_OVERRIDE := true
+
+# CPU governor support
+PERF_GOV_SUPPORTED := true
+PERF_DEFAULT_GOV := schedutil
+
+# AxionFX
+TARGET_INCLUDE_AXFX := true
